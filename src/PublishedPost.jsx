54897f5aeda49ce4a941153c965e3bbcecf32282
EyUp Student <eyupstudent@EyUps-MacBook-Air.local>
2023-04-18 12:25:50 +0100

    added working delete button

--- src/PublishedPost.jsx
+++ src/PublishedPost.jsx
@@ -1,5 +1,6 @@
 import {Button} from "@mui/material";
 import {useState} from "react";
+import {deletePostApi, dislikePostApi, likePostApi} from "./firebase-client";
 
 
 const PublishedPost = (props) => {
@@ -8,22 +9,35 @@ const PublishedPost = (props) => {
         return props.likes.filter((likers) => likers === props.username).length === 1
     }
 
+    const likePost = (post) => {
+        likePostApi(post, props.username)
+            .then(props.getPosts)
+    }
+
+    const unlikePost = (post) => {
+        dislikePostApi(post, props.username)
+            .then(props.getPosts)
+    }
+
     const clientLiked = clientHasLiked();
 
-    // const [clientLiked, setClientLiked] = useState(clientHasLiked())
-
-    const onClick = () => {
+    const likeBtn = () => {
         clientLiked ?
-            props.unlikePost(props.post, props.username) :
-            props.likePost(props.post, props.username);
-        // setClientLiked(clientHasLiked)
+            unlikePost(props.post, props.username) :
+            likePost(props.post, props.username);
+    }
+
+    const deleteBtn = () => {
+        deletePostApi(props.post.id)
+            .then(props.getPosts)
     }
 
     return (
         <div>
             <p>{props.text}</p>
             <p>👍 {props.likes.length}</p>
-            <Button onClick={onClick}>{clientLiked ? 'unlike' : 'like'}</Button>
+            <Button onClick={likeBtn}>{clientLiked ? 'unlike' : 'like'}</Button>
+            {props.post.username === props.username && <Button onClick={deleteBtn}>Delete</Button>}
         </div>
     )
 }
